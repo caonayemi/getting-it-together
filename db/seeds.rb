309c888e2b446f84a9074df7ced3be1ebb25df55
caonayemi <caonayemi@gmail.com>
2015-07-23 16:47:40 -0500

    Add pillar seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,11 @@ u4 = User.create!(
   password: 1234,
   email:   "u4@mail.com"
   )
+
+Pillar.create!(name: "Physical Togetherness", description: "I strive to take care of my body", priority: 1)
+Pillar.create!(name: , description: "I strive to take care of my emotions", priority: 2)
+Pillar.create!(name: , description: "I strive to take care of my space", priority: 3)
+Pillar.create!(name: , description: "I strive to take care of my relationships", priority: 4)
+Pillar.create!(name: , description: "I strive to prioritize intentionally", priority: 5)
+Pillar.create!(name: , description: "I strive to take care of my finances", priority: 6)
+Pillar.create!(name: , description: "I strive to be flexible", priority: 7)
