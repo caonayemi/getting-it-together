242b934656948a015645526ce1d3eec3577273e9
caonayemi <caonayemi@gmail.com>
2015-07-24 09:50:48 -0500

    Add user page and change seed data to have impending goal deadlines

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,5 @@
 get '/users/:user_id' do
+  @user = User.find(params[:user_id])
+  @goals = @user.goals
   erb :"users/show"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ pillars = Pillar.all
 strands = Strand.all
 
 20.times do
-  Goal.create!(setter: users.sample, name: (Faker::Hacker.verb + " " + Faker::Hacker.adjective + " " + Faker::Hacker.noun), pillar: pillars.sample, strand: strands.sample, set_at: DateTime.current, deadline: (DateTime.current + 30))
+  Goal.create!(setter: users.sample, name: (Faker::Hacker.verb + " " + Faker::Hacker.adjective + " " + Faker::Hacker.noun), pillar: pillars.sample, strand: strands.sample, set_at: DateTime.current, deadline: (DateTime.current + rand(1..5)))
 end
 
 goals = Goal.all
