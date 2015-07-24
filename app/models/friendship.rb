93518a94b4adc2e36098043a8d6f373938a15419
caonayemi <caonayemi@gmail.com>
2015-07-23 22:55:49 -0500

    Add questions about friendship associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -8,4 +8,6 @@ class Friendship < ActiveRecord::Base
   # Figure out how to validate that the initiator and acceptor are not the same user
 
   # Figure out how to validate that a friendship is not duplicated in reverse (initiator A and acceptor B is one friendship; initiator B and acceptor A cannot be another friendship)
+
+  # Should this become a "friendable" polymorphism?
 end
