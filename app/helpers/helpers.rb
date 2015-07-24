3203ff445dac2d35a2981e9594c242e801b63c93
caonayemi <caonayemi@gmail.com>
2015-07-23 22:15:56 -0500

    Edit nav bar and remove login/logout helper method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,13 +2,4 @@ helpers do
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def login_logout_button(user)
-    if user
-      "<a href='/logout'>Logout</a>"
-    else
-      "<a href='/login'>Login</a>"
-    end
-  end
-
 end
